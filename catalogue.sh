c38e26278b986a4203ed7ae649a1291f71fa7525
rajesh <rajeshsharma987987@gmail.com>
2023-06-05 13:21:54 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,27 +1,19 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
-
-yum install nodejs -y  &>>/tmp/roboshop.log
-
-useradd roboshop  &>>/tmp/roboshop.log
-rm -rf /app  &>>/tmp/roboshop.log
-
-mkdir /app  &>>/tmp/roboshop.log
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-
-unzip /tmp/catalogue.zip  &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip
 cd /app
+npm install
+cp ctalogue.service /etc/system/catalogue.service
 
-npm install  &>>/tmp/roboshop.log
-cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service  &>>/tmp/roboshop.log
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
 
-systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable catalogue  &>>/tmp/roboshop.log
-systemctl start catalogue  &>>/tmp/roboshop.log
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
 
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo  &>>/tmp/roboshop.log
-
-yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
-
-mongo --host mongodb-dev.devops73.site </app/schema/catalogue.js  &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.devops73.site </app/schema/catalogue.js
